9de8515d7bd9de3a00eaf6134d4bb6d991b611fb
Ondrej Chrastina <chrastina.ondra@gmail.com>
2019-05-28 21:10:33 +0200

    revert gitignore

--- source/plugins/gatsby-source-kentico-cloud/config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-const customTrackingHeader = {
-  header: 'X-KC-SOURCE',
-  value: 'gatsby-source-kentico-cloud;3.1.0-beta-gatsby-preview'
-};
-module.exports = {
-  customTrackingHeader
-};
