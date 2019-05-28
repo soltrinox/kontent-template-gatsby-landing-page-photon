5320c67890a10b86552e4cb1e24d5feec0f9ec1a
Ondrej Chrastina <chrastina.ondra@gmail.com>
2019-05-28 20:59:59 +0200

    add built files

---
+++ source/plugins/gatsby-source-kentico-cloud/gatsby-node.js
@@ -0,0 +1,110 @@
+"use strict";
+
+require(`@babel/polyfill`);
+
+const _ = require(`lodash`);
+
+const {
+  DeliveryClient
+} = require(`kentico-cloud-delivery`);
+
+const validation = require(`./validation`);
+
+const itemNodes = require('./itemNodes');
+
+const typeNodes = require('./typeNodes');
+
+const languageVariantsDecorator = require('./decorators/languageVariantsDecorator');
+
+const typeItemDecorator = require('./decorators/typeItemDecorator');
+
+const linkedItemsElementDecorator = require('./decorators/linkedItemsElementDecorator');
+
+const richTextElementDecorator = require('./decorators/richTextElementDecorator');
+
+const {
+  customTrackingHeader
+} = require('./config');
+
+exports.sourceNodes = async ({
+  actions: {
+    createNode
+  },
+  createNodeId
+}, {
+  deliveryClientConfig,
+  languageCodenames
+}) => {
+  console.info(`Generating Kentico Cloud nodes for projectId:\
+ ${_.get(deliveryClientConfig, 'projectId')}`);
+  console.info(`Provided language codenames: ${languageCodenames}.`);
+  console.info(`GATSBY PREVIEW DEVELOPMENT VERSION`);
+  validation.validateLanguageCodenames(languageCodenames);
+  const defaultLanguageCodename = languageCodenames[0];
+  const nonDefaultLanguageCodenames = languageCodenames.slice(1);
+  addHeader(deliveryClientConfig, customTrackingHeader);
+  const client = new DeliveryClient(deliveryClientConfig);
+  const contentTypeNodes = await typeNodes.get(client, createNodeId);
+  const defaultCultureContentItemNodes = await itemNodes.getFromDefaultLanguage(client, defaultLanguageCodename, contentTypeNodes, createNodeId);
+  const nonDefaultLanguageItemNodes = await itemNodes.getFromNonDefaultLanguage(client, nonDefaultLanguageCodenames, contentTypeNodes, createNodeId);
+  languageVariantsDecorator.decorateItemsWithLanguageVariants(defaultCultureContentItemNodes, nonDefaultLanguageItemNodes);
+  const allItemNodes = defaultCultureContentItemNodes.concat(_.flatten(nonDefaultLanguageItemNodes.values()));
+  typeItemDecorator.decorateTypeNodesWithItemLinks(allItemNodes, contentTypeNodes);
+  linkedItemsElementDecorator.decorateItemNodesWithLinkedItemsLinks(defaultCultureContentItemNodes, nonDefaultLanguageItemNodes);
+  richTextElementDecorator.decorateItemNodesWithRichTextLinkedItemsLinks(defaultCultureContentItemNodes, nonDefaultLanguageItemNodes);
+  console.info(`Creating content type nodes.`);
+  createNodes(contentTypeNodes, createNode);
+  console.info(`Creating content item nodes for default language.`);
+  createNodes(defaultCultureContentItemNodes, createNode);
+  console.info(`Creating content item nodes for non-default languages.`);
+  nonDefaultLanguageItemNodes.forEach(languageNodes => {
+    createNodes(languageNodes, createNode);
+  });
+  console.info(`Kentico Cloud nodes generation finished.`);
+  return;
+};
+/**
+ *
+ * @param {DeliveryClientConfig} deliveryClientConfig
+ *  Kentico Cloud JS configuration object
+ * @param {IHeader} trackingHeader tracking header name
+ */
+
+
+const addHeader = (deliveryClientConfig, trackingHeader) => {
+  let headers = deliveryClientConfig.globalHeaders ? _.cloneDeep(deliveryClientConfig.globalHeaders) : [];
+
+  if (headers.some(header => header.header === trackingHeader.header)) {
+    console.warn(`Custom HTTP header value with name ${trackingHeader.header}
+      will be replaced by the source plugin.
+      Use different header name if you want to avoid this behavior;`);
+    headers = headers.filter(header => {
+      return header.header !== trackingHeader.header;
+    });
+  }
+
+  headers.push({
+    header: trackingHeader.header,
+    value: trackingHeader.value
+  });
+  deliveryClientConfig.globalHeaders = headers;
+};
+/**
+ * Call @see createNode function  for all items in @see nodes.
+ * @param {Array} nodes Gatsby nodes to create
+ * @param {Function} createNode Gatsby API method for Node creation.
+ */
+
+
+const createNodes = (nodes, createNode) => {
+  try {
+    nodes.forEach(contentTypeNode => {
+      const nodeId = contentTypeNode.id;
+      const nodeCodeName = contentTypeNode.system.codename;
+      console.info(`Creating node: ${nodeId}(${nodeCodeName})`);
+      createNode(contentTypeNode);
+    });
+  } catch (error) {
+    console.error(`Error when creating nodes. Details: ${error}`);
+  }
+};
---
+++ source/plugins/gatsby-source-kentico-cloud/normalize.js
@@ -0,0 +1,103 @@
+"use strict";
+
+const _ = require(`lodash`);
+
+const crypto = require(`crypto`);
+
+const changeCase = require(`change-case`);
+/**
+ * Parses a content item to rebuild the 'elements' property.
+ * @param {object} contentItem - The content item to be parsed.
+ * @param {array} processedContents - The array with the recursion
+ * traversal history.
+ * @return {object} Parsed content item.
+ * @throws {Error}
+ */
+
+
+const parseContentItemContents = (contentItem, processedContents = []) => {
+  if (processedContents.includes(contentItem.system.codename)) {
+    processedContents.push(contentItem.system.codename);
+    const flatted = processedContents.join(` -> `);
+    console.error(`Cycle detected in linked items' path: ${flatted}`);
+    return {
+      system: contentItem.system,
+      elements: null,
+      cycleDetected: true
+    };
+  }
+
+  processedContents.push(contentItem.system.codename);
+  const elements = {};
+  const elementPropertyKeys = Object.keys(contentItem).filter(key => key !== `system` && key !== `elements`);
+
+  for (const key of elementPropertyKeys) {
+    let propertyValue;
+
+    if (_.get(contentItem, `elements[${key}].type`) === 'modular_content') {
+      const linkedItems = [];
+      contentItem[key].forEach(linkedItem => {
+        linkedItems.push(parseContentItemContents(linkedItem, Array.from(processedContents), contentItem));
+      });
+      propertyValue = linkedItems;
+    } else {
+      propertyValue = contentItem[key];
+    }
+
+    elements[key] = propertyValue;
+  }
+
+  const itemWithElements = {
+    system: contentItem.system,
+    elements: elements
+  };
+  return itemWithElements;
+};
+/**
+ * Create Gatsby Node structure.
+ * @param {Number} nodeId Gebnerated Gatsby node ID.
+ * @param {Object} kcArtifact Node's Kentico Cloud data.
+ * @param {String} artifactKind Type of the artifact ('item/type')
+ * @param {String} codeName Item code name
+ * @param {Object} additionalNodeData Additional data
+ * @return {Object} Gatsby node object
+ */
+
+
+const createKcArtifactNode = (nodeId, kcArtifact, artifactKind, codeName = ``, additionalNodeData = null) => {
+  const nodeContent = JSON.stringify(kcArtifact);
+  const nodeContentDigest = crypto.createHash(`md5`).update(nodeContent).digest(`hex`);
+  const codenamePascalCase = changeCase.pascalCase(codeName);
+  const artifactKindPascalCase = changeCase.pascalCase(artifactKind);
+  return { ...kcArtifact,
+    ...additionalNodeData,
+    id: nodeId,
+    parent: null,
+    children: [],
+    usedByContentItems___NODE: [],
+    internal: {
+      type: `KenticoCloud${artifactKindPascalCase}${codenamePascalCase}`,
+      content: nodeContent,
+      contentDigest: nodeContentDigest
+    }
+  };
+};
+
+const addLinkedItemsLinks = (itemNode, linkedNodes, linkPropertyName, originalNodeCollection = []) => {
+  linkedNodes.forEach(linkedNode => {
+    if (!linkedNode.usedByContentItems___NODE.includes(itemNode.id)) {
+      linkedNode.usedByContentItems___NODE.push(itemNode.id);
+    }
+  }); // important to have the same order as it is Kentico Cloud
+
+  const sortPattern = originalNodeCollection.map(item => item.system.id);
+  const sortedLinkedNodes = linkedNodes.sort((a, b) => sortPattern.indexOf(a.system.id) - sortPattern.indexOf(b.system.id)).map(item => item.id);
+
+  _.set(itemNode.elements, linkPropertyName, sortedLinkedNodes);
+};
+
+module.exports = {
+  createKcArtifactNode,
+  addLinkedItemsLinks,
+  parseContentItemContents
+};
