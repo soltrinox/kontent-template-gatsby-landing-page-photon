e2fe36ef1fe38ab50919512db60c0334538081e2
abhiaiyer91 <abhiaiyer91@gmail.com>
2019-05-29 14:57:16 -0500

    trying to debug

---
+++ source/plugins/gatsby-source-kentico-cloud/config.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const customTrackingHeader = {
+  header: 'X-KC-SOURCE',
+  value: 'gatsby-source-kentico-cloud;3.1.0-beta-gatsby-preview'
+};
+module.exports = {
+  customTrackingHeader
+};
